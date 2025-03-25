ebd2d3a7436f92155d4504d8d982f2bf07121a61
Eduardo Fawke <teduardof@gmail.com>
2025-03-25 22:59:45 +0000

    s

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -21,7 +21,7 @@ app.use('/uploads', express.static(path.join(__dirname, './public/uploads')));
 let port:number = process.env.PORT ? parseInt(process.env.PORT) : 8000;
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('../frontend/build'));
+  app.use(express.static('../../frontend/build'));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../../', 'frontend', 'build', 'index.html'));
   });
