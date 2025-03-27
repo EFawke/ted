896db3d6f8e20806d768889c409523656db58528
Eduardo Fawke <teduardof@gmail.com>
2025-03-27 01:15:59 +0000

    s

--- frontend/src/GoogleLoginButton.js
+++ frontend/src/GoogleLoginButton.js
@@ -1,15 +1,22 @@
+import React, { useEffect } from 'react';
 import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google';
 import axios from 'axios';
-import React from 'react';
 import { useAuth } from './AuthContext.js';
-
 
 const GoogleLoginButton = () => {
   const { login } = useAuth();
   const isProduction = process.env.NODE_ENV === 'production';
   const baseURL = isProduction
-  ? 'https://tedfawke.com/'
-  : 'http://localhost:8000/';
+    ? 'https://tedfawke.com/'
+    : 'http://localhost:8000/';
+
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://accounts.google.com/gsi/client';
+    script.async = true;
+    script.defer = true;
+    document.body.appendChild(script);
+  }, []);
 
   const responseMessage = async (response) => {
     try {
@@ -30,19 +37,19 @@ const GoogleLoginButton = () => {
   };
 
   return (
-    <GoogleOAuthProvider clientId="128885006939-iihg8o4bg4nfb1lt76bhb23t4ustdr2l.apps.googleusercontent.com">
-      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: 44 }}>
-      <GoogleLogin
-        onSuccess={responseMessage}
-        onError={errorMessage}
-        theme="outline"
-        size="large"
-        text="signin_with"
-        shape="rectangular"
-        width="100%"
-      />
-      </div>
-    </GoogleOAuthProvider>
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '44px' }}>
+      <GoogleOAuthProvider clientId="128885006939-iihg8o4bg4nfb1lt76bhb23t4ustdr2l.apps.googleusercontent.com">
+        <GoogleLogin
+          onSuccess={responseMessage}
+          onError={errorMessage}
+          theme="outline"
+          size="large"
+          text="signin_with"
+          shape="rectangular"
+          width={280} // use a valid pixel value here
+        />
+      </GoogleOAuthProvider>
+    </div>
   );
 };
 
