8766df8eef361bd1b202b0da22afd94b3446fe5e
Eduardo Fawke <teduardof@gmail.com>
2025-04-05 00:14:11 +0100

    s

--- backend/src/api/uploadRouter.ts
+++ backend/src/api/uploadRouter.ts
@@ -26,8 +26,6 @@ const upload = multer({ storage });
 uploadRouter.post('/', upload.single('image'), (req, res) => {
   if (req.file) {
     console.log("Uploaded file path:", req.file.path);
-
-    // Send back a URL that can be accessed in the browser
     res.status(200).json({ url: `/uploads/${req.file.filename}` });
   } else {
     res.status(500).json({ error: "Error uploading file" });
--- frontend/src/GoogleLoginButton.js
+++ frontend/src/GoogleLoginButton.js
@@ -6,12 +6,12 @@ import { useAuth } from './AuthContext.js';
 import './App.css';
 
 const GoogleLoginButton = () => {
-  // const [isMounted, setIsMounted] = useState(false);
+  const [isMounted, setIsMounted] = useState(false);
 
-  // useEffect(() => {
-  //   setIsMounted(true);
-  //   return () => setIsMounted(false);
-  // }, []);
+  useEffect(() => {
+    setIsMounted(true);
+    return () => setIsMounted(false);
+  }, []);
 
   useEffect(() => {
     if (typeof window !== 'undefined' && window.google === undefined) {
@@ -24,32 +24,37 @@ const GoogleLoginButton = () => {
     ? 'https://tedfawke.com/'
     : 'http://localhost:8000/';
 
-  const responseMessage = async (response) => {
-    try {
-      const { data } = await axios.post(
-        `${baseURL}auth/google`,
-        { token: response.credential }
-      );
-      login(data.user, data.token);
-      window.location.reload();
-    } catch (error) {
-      console.error('Login failed:', error);
-    }
-  };
+  // const responseMessage = async (response) => {
+  //   try {
+  //     const { data } = await axios.post(
+  //       `${baseURL}auth/google`,
+  //       { token: response.credential }
+  //     );
+  //     login(data.user, data.token);
+  //     window.location.reload();
+  //   } catch (error) {
+  //     console.error('Login failed:', error);
+  //   }
+  // };
 
-  const errorMessage = (error) => {
-    console.log('Login Failed:', error);
-  };
+  // const errorMessage = (error) => {
+  //   console.log('Login Failed:', error);
+  // };
 
   return (
     <GoogleOAuthProvider clientId="128885006939-iihg8o4bg4nfb1lt76bhb23t4ustdr2l.apps.googleusercontent.com">
-      <div>
-      {/* {isMounted && ( */}
+      <div className="google-auth-wrapper">
+      {isMounted && (
         <GoogleLogin
-          onSuccess={responseMessage}
-          onError={errorMessage}
+          // onSuccess={responseMessage}
+          // onError={errorMessage}
+          // use_fedcm_supported={false}
+          // text="signin_with"
+          // shape="rectangular"
+          // size="large"
+          // width="300" // Adjust or use '100%' for full container width
           />
-        {/* )} */}
+        )}
       </div>
     </GoogleOAuthProvider>
   );
